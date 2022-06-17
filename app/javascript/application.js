66d1393066050da05d5ac0fc5e9f668b64137eaf
hungpm98 <hungpm@HOANGs-Mac-mini.local>
2022-06-17 17:08:07 +0700

    dashboard and logout devise

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "jquery"
+import "custom/sb-admin-2"
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
   namespace :admin do
     root "home#index"
     devise_for :admins, path:"", path_names: {sign_in: "login", sign_out: "logout"}, skip: [:registations, :passwords]
