a54037f7cf612d317960e380ca201b20cca5dc86
hungpm98 <hungpm@HOANGs-Mac-mini.local>
2022-06-17 17:59:58 +0700

    migration posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_040058) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_104106) do
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_17_040058) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id", "created_at"], name: "index_posts_on_admin_id_and_created_at"
+    t.index ["admin_id"], name: "index_posts_on_admin_id"
+  end
+
+  add_foreign_key "posts", "admins"
 end
